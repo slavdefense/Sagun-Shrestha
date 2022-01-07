1fb63d123f6efbcd5c924cae3b575f1131e24848
Sagun Shrestha <goodsamaritan.4846@gmail.com>
2022-01-06 20:04:26 -0800

    Stub up python-django app on the portfolio

---
+++ src/Pages/Work/Carrental/Carrental.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+// import {Player} from 'video-react'
+
+const CarRental = () => {
+  return (
+    <div>
+      <div className="backlog-container">
+        <div className="backlog-info">
+          <h1>
+            Air BnB for Cars
+            <p className="italic">A Python-Django application</p>
+          </h1>
+          <ul>
+            <li>Python, Django, CSS, HTML5,</li>
+            <li>Posgresql, AWS, Django Auth</li>
+            <li>Multer, Whimsical</li>
+          </ul>
+          <div className="site-github">
+            <a
+              className="site-github-atag"
+              href="https://rentorlistcars.herokuapp.com/"
+            >
+              {" "}
+              Visit Site
+            </a>
+            <div className="arrimg">
+              <img src="/arr1.png" alt="" />
+            </div>
+
+            <a
+              className="site-github-atag-octocat"
+              href="https://github.com/slavdefense/Car-rental-made-easy"
+            >
+              <img src="/Octocat.png" alt="" />
+            </a>
+          </div>
+        </div>
+        <div className="backlog-img">
+          <video className="video" autoPlay muted loop>
+            <source src="/carrent.mp4" type="video/mp4"></source>
+          </video>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CarRental;
--- src/Pages/Work/Work.jsx
+++ src/Pages/Work/Work.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import BackLog from './Backlog/Backlog';
 import MountainBikeTrail from './Mountainbike-trail/Mountainbike-trail';
+import CarRental from './Carrental/Carrental';
 import Slot from './Slot/Slot';
 import ScrollToTop from '../../Components/ScrollToTop/ScrollToTopOnMount';
 import './Work.css'
@@ -9,6 +10,7 @@ const Work = () => {
     <div id="Work">
      
       <BackLog/>
+      <CarRental/>
       <MountainBikeTrail/>
       <Slot/>
       
